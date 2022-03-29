ebf645887b62f05dccbc2388f5c743ccfc21abf0
Michael Babb <babbm@uw.edu>
2022-03-28 17:18:16 -0700

    upload batch file

---
+++ code/create_passphrase.bat
@@ -0,0 +1,9 @@
+rem: generate a passphrase from a list of words
+
+rem: activate the anaconda environment
+call C:\Users\babbm\Anaconda3\Scripts\activate.bat C:\Users\babbm\Anaconda3
+rem: call conda activate analysis
+
+python H:\git\passphrase\code\generate_passphrase.py | clip
+
+
